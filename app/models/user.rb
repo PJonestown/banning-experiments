ea943b7711f2137231375b5ed55ae814eb911f63
Newton <anthpaul0@gmail.com>
2015-04-16 13:33:43 -0500

    login form for sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-  validates :user, presence: true, uniqueness: { case_sensitive: false }, :length => { maximum: 20 }
+   validates :name, presence: true, uniqueness: { case_sensitive: false },
+     :length => { maximum: 20 }
+
 end
